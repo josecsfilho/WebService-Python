64b95b278409fabf53b82d878cfb36d5e1074557
José Filho <100032235+josecsfilho@users.noreply.github.com>
2023-08-31 22:47:13 -0300

    deploy

--- webservice/settings.py
+++ webservice/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
